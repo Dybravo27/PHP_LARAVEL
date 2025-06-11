2686a9092bd2a5cd5ed63e12e33da7adf7c7c0c6
Dybravo27 <dylanslebyng@gmail.com>
2025-06-11 18:14:19 -0500

    Manejo de Eloquent relaciones

---
+++ app/Models/Image.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Image extends Model
+{
+    use HasFactory;
+    public function products()
+    {
+        return $this->hasOne('Product');
+    }
+    public function productImage()
+    {
+        return $this->hasOneThrough(Product::class, Category::class);
+    }
+}
